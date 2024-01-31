a306dd807108b31638953d9b483b6943712c4fc9
Anton Hirov <hirov.anton@yahoo.com>
2024-01-31 05:13:14 +0200

    Added test projects

---
+++ Tests/IO/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace FileDiff.IO.Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
